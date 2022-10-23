f57d9e3de6768a2ff533b6aea259e425463a471a
santiagofer4 <santiagofernandezdelasierra@gmail.com>
2022-10-23 18:50:18 -0300

    adding cart context and shopping icon as well as cart dropdown

--- src/index.js
+++ src/index.js
@@ -3,18 +3,25 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import { CartProvider } from './contexts/cart.context';
+import { ProductsProvider } from './contexts/products.context';
 import { UserProvider } from './contexts/user.context';
+
 
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </BrowserRouter>
+  <BrowserRouter>
+    <UserProvider>
+      <ProductsProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </BrowserRouter>
   //</React.StrictMode>
 );
 
--- src/routes/navigation/navigation-component.jsx
+++ src/routes/navigation/navigation-component.jsx
@@ -1,13 +1,19 @@
 import React, { Fragment, useContext } from 'react'
 import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as CrwnLogo} from '../../assets/crown.svg'
+import { CartDropdown } from '../../components/cart-dropdown/cart-dropdown.component';
+import { CartIcon } from '../../components/cart-icon/cart-icon.component';
+import { CartContext } from '../../contexts/cart.context';
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 import './navigation.styles.scss'
 
+
+
 const Navigation = () => {
 
   const { currentUser } = useContext(UserContext)
+  const { isCartOpen } = useContext(CartContext)
 
   return (
     <Fragment>
@@ -25,13 +31,16 @@ const Navigation = () => {
             : (<Link className="nav-link" to="/auth">SIGN IN</Link>)
           }
 
+          <CartIcon />
         </div>
 
+        {isCartOpen && <CartDropdown/>}
+
       </div>
+
       <Outlet />
     </Fragment>
   );
-
 
 }
 
